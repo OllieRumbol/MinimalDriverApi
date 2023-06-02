4c0196a89e3dbfc6b11530a0bb591470d065da2c
Oliver Bourne <otgb97@gmail.com>
2023-06-02 14:08:39 +0100

    Create data access layer

---
+++ MinimalDriverAPIApp/DataAccess/Data/DriverData.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Models;
+
+namespace DataAccess.Data;
+
+public class DriverData : IDriverData
+{
+    private readonly ISqlDataAccess _db;
+
+    public DriverData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<IEnumerable<DriverModel>> GetDrivers() => _db.LoadData<DriverModel, dynamic>("dbo.spDriver_GetAll", new { });
+
+    public async Task<DriverModel?> GetDriver(int id)
+    {
+        var results = await _db.LoadData<DriverModel, dynamic>("dbo.spDriver_Get", new { Id = id });
+
+        return results.FirstOrDefault();
+    }
+
+    public Task InsertDriver(DriverModel driver) => _db.SaveData("dbo.spDriver_Insert", new { driver.FirstName, driver.LastName });
+
+    public Task UpdateDriver(DriverModel driver) => _db.SaveData("dbo.spDriver_Update", driver);
+
+    public Task DeleteDriver(int id) => _db.SaveData("dbo.spDriver_Delete", new { Id = id });
+}
