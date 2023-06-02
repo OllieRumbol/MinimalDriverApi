2937e309dde7c453893fc9f0008661b4eba655f6
Oliver Bourne <otgb97@gmail.com>
2023-06-02 14:09:02 +0100

    Create minimal api endpoints

---
+++ MinimalDriverAPIApp/MinimalDriverAPI/Api.cs
@@ -0,0 +1,82 @@
+﻿namespace MinimalDriverAPI;
+
+public static class Api
+{
+    public static void ConfigureApi(this WebApplication app)
+    {
+        app.MapGet("/Drivers", GetDrivers);
+        app.MapGet("/Drivers/{id}", GetDriver);
+        app.MapPost("/Drivers", InsertDriver);
+        app.MapPut("/Drivers", UpdateDriver);
+        app.MapDelete("/Driver/{id}", DeleteDriver);
+    }
+
+    private static async Task<IResult> GetDrivers(IDriverData data)
+    {
+        try
+        {
+            return Results.Ok(await data.GetDrivers());
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> GetDriver(int id, IDriverData data)
+    {
+        try
+        {
+            var results = await data.GetDriver(id);
+            if (results is null)
+            {
+                return Results.NotFound();
+            }
+
+            return Results.Ok(results);
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> InsertDriver(DriverModel driver, IDriverData data)
+    {
+        try
+        {
+            await data.InsertDriver(driver);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> UpdateDriver(DriverModel driver, IDriverData data)
+    {
+        try
+        {
+            await data.UpdateDriver(driver);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> DeleteDriver(int id, IDriverData data)
+    {
+        try
+        {
+            await data.DeleteDriver(id);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+}
---
+++ MinimalDriverAPIApp/MinimalDriverAPI/Program.cs
@@ -0,0 +1,22 @@
+using DataAccess.DbAccess;
+using MinimalDriverAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+builder.Services.AddSingleton<IDriverData, DriverData>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.ConfigureApi();
+
+app.Run();
