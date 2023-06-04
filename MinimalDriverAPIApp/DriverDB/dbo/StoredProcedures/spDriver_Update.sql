39ab11f4dc627708f6600463fdc6914c19174ac8
Oliver Bourne <otgb97@gmail.com>
2023-06-04 22:56:22 +0100

    adding new fields

--- MinimalDriverAPIApp/DriverDB/dbo/StoredProcedures/spDriver_Update.sql
+++ MinimalDriverAPIApp/DriverDB/dbo/StoredProcedures/spDriver_Update.sql
@@ -1,10 +1,12 @@
 ﻿CREATE PROCEDURE [dbo].[spDriver_Update]
 	@Id int,
 	@FirstName nvarchar(50),
-	@LastName nvarchar(50)
+	@LastName nvarchar(50),
+	@DateOfBirth date,
+	@DrivingLicenceNumber nvarchar(20)
 AS
 begin
 	UPDATE dbo.Driver
-	SET FirstName = @FirstName,  LastName = @LastName
+	SET FirstName = @FirstName,  LastName = @LastName, DateOfBirth = @DateOfBirth, @DrivingLicenceNumber = @DrivingLicenceNumber
 	WHERE Id = @Id
 end
